33ca4671204958ed9a79dbf27d52e1525e05ca8a
NarekHakobyanJS <haobyan95narek@gmail.com>
2024-10-12 14:54:39 +0400

    use mongo server

---
+++ db.js
@@ -0,0 +1,34 @@
+const {MongoClient} = require('mongodb');
+
+// BASEURL + DB
+const url = 'mongodb://localhost:27017/person';
+
+// 2-րդ ֆունկցիայի փոփոխականը
+// որին կվերագրենք DB միացման արդյունքը
+let dbConnection;
+
+// երկու ֆունկցիա դուրս ենք հանում
+// և օգտագործում ենք ֆայլում
+module.exports = {
+    // connectToDb ֆունկցիան որպես արգումենտ ստանումա callBack
+    connectToDb : (callBack) => {
+        //MongoClient կլասսի connect ֆունկցիան կանչենք որը անսինխռոնա
+        // և ստանում որպես արգումենտ url և վերադարձնում է promise
+        MongoClient
+            .connect(url)
+            // բարեհաջող միացման դեպքում then-ին 
+            // որպես արգումենտ գալիսա հատուկ օբյեկտ client անունով
+            .then((client) => {
+                console.log('successful connection');
+                // վերևում հայտարաարված dbConnection փոփոխականին
+                // վերագրում ենք client օբյեկտի db ֆունկցիաի կանչը
+                dbConnection = client.db()
+                // վերադարձնում ենք connectToDb ֆունկցիաի
+                // ստացած արգումենտ callBack ը
+                callBack()
+            })
+            .catch((err) => console.log(err))
+    },
+    //getDb ֆունկցիան կվերադարձնի Db միացման արդյունքը
+    getDb : () => dbConnection
+}
---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const { connectToDb, getDb } = require('./db.js')
+const app = express()
+const port = 3000
+
+//db փոփոխականին վերագրվելու է 
+// getDb ֆունկցիայի կանչը որը ստեղծել ենք db ֆայլում
+// և որը հետեր տալիս DataBase-ը
+let db
+
+//connectToDb ի callBack ֆունկցիան որպես արգումենտ ստանում է error - ը
+connectToDb((err) => {
+    if (!err) {
+        //եթե error-ը չկա մենք միացնումենք ծրագրին լսելը
+        app.listen(port, () => {
+            console.log(`Example app listening on port ${port}`)
+        })
+        // express - ի միացումը բազային
+        db = getDb()
+    } else {
+        console.log('Db connection error');  
+    }
+})
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
